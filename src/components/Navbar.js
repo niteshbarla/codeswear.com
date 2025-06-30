51054f8b92f5a4a37accc4ce46b5f435c752535e
Nitesh Barla <nitesh.triptrova2019@gmail.com>
2025-06-30 22:35:13 +0530

    Updated the cart icons and options

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,20 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { FaCartArrowDown } from "react-icons/fa";
+import { IoIosCloseCircle } from "react-icons/io";
+import { useRef } from "react";
 
 const Navbar = () => {
+  const ref = useRef();
+
+  const toggleCart = () => {
+    if (ref.current.style.transform === "translateX(0px)") {
+      ref.current.style.transform = "translateX(100%)";
+    } else {
+      ref.current.style.transform = "translateX(0px)";
+    }
+  };
+
   return (
     <div className="flex flex-col md:flex-row justify-center md:justify-start items-center bg-gray-50 font-bold sticky top-0 z-50 p-0 my-0 shadow-md">
       <Link href={"/"} legacyBehavior>
@@ -32,11 +44,26 @@ const Navbar = () => {
           </Link>
         </ul>
       </div>
-      <div className="cart absolute right-0 top-10 mx-5">
+      <div onClick={toggleCart} className="cart absolute right-0 top-10 mx-5">
         <FaCartArrowDown className="text-xl md:text-2xl cursor-pointer" />
       </div>
-      <div className="sidebar absolute top-0 right-0 bg-green-400">
+      <div
+        ref={ref}
+        className="sideCart absolute top-0 right-0 bg-green-200 p-10 transition-transform translate-x-full"
+      >
+        <h2 className="font-bold text-xl">Shopping Cart</h2>
+        <span
+          onClick={toggleCart}
+          className="absolute top-2 right-2 text-2xl cursor-pointer text-green-800"
+        >
+          <IoIosCloseCircle />
+        </span>
         This is my shopping cart
+        <ol>
+          <li>
+            <span>Tshirt - Wear the Code</span>
+          </li>
+        </ol>
       </div>
     </div>
   );
