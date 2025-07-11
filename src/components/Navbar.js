6678b0a497b0e70a5db033ec419d8d19fbfb3a70
Nitesh Barla <nitesh.triptrova2019@gmail.com>
2025-07-11 21:09:12 +0530

    Updated the sidecarts with plus and minus button and also added checkout button with cool icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,8 @@ import Link from "next/link";
 import { FaCartArrowDown } from "react-icons/fa";
 import { IoIosCloseCircle } from "react-icons/io";
 import { useRef } from "react";
-import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
+import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
+import { BsFillBagCheckFill } from "react-icons/bs";
 
 const Navbar = () => {
   const ref = useRef();
@@ -52,7 +53,7 @@ const Navbar = () => {
       </div>
       <div
         ref={ref}
-        className="w-72 sideCart absolute top-0 right-0 bg-green-200 p-10 transition-transform translate-x-full px-8 py-10 h-screen"
+        className="w-72 sideCart absolute top-0 right-0 bg-green-100 p-10 transition-transform translate-x-full px-8 py-10 h-screen"
       >
         <h2 className="font-bold text-xl text-center">Shopping Cart</h2>
         <span
@@ -66,44 +67,57 @@ const Navbar = () => {
             <div className="item flex my-5">
               <div className="w-2/3 font-semibold">Tshirt - Wear the Code</div>
               <div className="flex font-semibold items-center justify-center w-1/3 text-lg">
-                <CiCircleMinus /> <span className="mx-2">1</span>
-                <CiCirclePlus />
+                <FaMinusCircle className="cursor-pointer text-green-900" />{" "}
+                <span className="mx-2">1</span>
+                <FaPlusCircle className="cursor-pointer text-green-900" />
               </div>
             </div>
           </li>
           <li>
             <div className="item flex my-5">
               <div className="w-2/3 font-semibold">Tshirt - Wear the Code</div>
-              <div className="flex font-semibold items-center justify-center w-1/3">
-                1
+              <div className="flex font-semibold items-center justify-center w-1/3 text-lg">
+                <FaMinusCircle className="cursor-pointer text-green-900" />{" "}
+                <span className="mx-2">1</span>
+                <FaPlusCircle className="cursor-pointer text-green-900" />
               </div>
             </div>
           </li>
           <li>
             <div className="item flex my-5">
               <div className="w-2/3 font-semibold">Tshirt - Wear the Code</div>
-              <div className="flex font-semibold items-center justify-center w-1/3">
-                1
+              <div className="flex font-semibold items-center justify-center w-1/3 text-lg">
+                <FaMinusCircle className="cursor-pointer text-green-900" />{" "}
+                <span className="mx-2">1</span>
+                <FaPlusCircle className="cursor-pointer text-green-900" />
               </div>
             </div>
           </li>
           <li>
             <div className="item flex my-5">
               <div className="w-2/3 font-semibold">Tshirt - Wear the Code</div>
-              <div className="flex font-semibold items-center justify-center w-1/3">
-                1
+              <div className="flex font-semibold items-center justify-center w-1/3 text-lg">
+                <FaMinusCircle className="cursor-pointer text-green-900" />{" "}
+                <span className="mx-2">1</span>
+                <FaPlusCircle className="cursor-pointer text-green-900" />
               </div>
             </div>
           </li>
           <li>
             <div className="item flex my-5">
               <div className="w-2/3 font-semibold">Tshirt - Wear the Code</div>
-              <div className="flex font-semibold items-center justify-center w-1/3">
-                1
+              <div className="flex font-semibold items-center justify-center w-1/3 text-lg">
+                <FaMinusCircle className="cursor-pointer text-green-900" />{" "}
+                <span className="mx-2">1</span>
+                <FaPlusCircle className="cursor-pointer text-green-900" />
               </div>
             </div>
           </li>
         </ol>
+        <button className="flex mx-auto mt-16 text-white bg-green-800 border-0 py-2 px-6 focus:outline-none hover:bg-green-900 rounded text-sm">
+          <BsFillBagCheckFill className="m-1" />
+          Checkout
+        </button>
       </div>
     </div>
   );
