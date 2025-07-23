885d827716cb834bec3f1abb82345c8d29168b89
Nitesh Barla <nitesh.triptrova2019@gmail.com>
2025-07-23 21:56:34 +0530

    Updated the _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,28 @@ import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  const [cart, setCart] = useState({});
+  const [subTotal, setSubTotal] = useState(0);
+
+  const saveCart = (myCart) => {
+    localStorage.setItem('cart', myCart);
+  } 
+  const addtoCart = (itemCode, qty, price, name, size, variant) => {
+    let newCart = cart;
+    if (itemCode in cart) {
+      newCart[itemCode].qty = cart[itemCode].qty + qty; 
+    }
+    else {
+      newCart[itemCode] = {qty: 1, price, name, size, variant};
+    
+    setCart(newCart);
+    saveCart(newCart);
+  }
+
+  const clearCart = () => {
+    setCart({});
+    saveCart({});
+  }
   return (
     <>
       <Navbar />
