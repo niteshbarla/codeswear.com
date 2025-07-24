bbff117a13b1b75c7080836678716483d9445d9b
Nitesh Barla <nitesh.triptrova2019@gmail.com>
2025-07-24 07:33:52 +0530

    Updated the _app.js to save ane remove the Cart

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,30 +1,42 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
   const [cart, setCart] = useState({});
   const [subTotal, setSubTotal] = useState(0);
 
   const saveCart = (myCart) => {
-    localStorage.setItem('cart', myCart);
-  } 
-  const addtoCart = (itemCode, qty, price, name, size, variant) => {
+    localStorage.setItem("cart", myCart);
+  };
+  const addToCart = (itemCode, qty, price, name, size, variant) => {
     let newCart = cart;
     if (itemCode in cart) {
-      newCart[itemCode].qty = cart[itemCode].qty + qty; 
+      newCart[itemCode].qty = cart[itemCode].qty + qty;
+    } else {
+      newCart[itemCode] = { qty: 1, price, name, size, variant };
     }
-    else {
-      newCart[itemCode] = {qty: 1, price, name, size, variant};
-    
     setCart(newCart);
     saveCart(newCart);
-  }
+  };
 
   const clearCart = () => {
     setCart({});
     saveCart({});
-  }
+  };
+
+  const removeFromCart = (itemCode, qty, price, name, size, variant) => {
+    let newCart = cart;
+    if (itemCode in cart) {
+      newCart[itemCode].qty = cart[itemCode].qty - qty;
+    } else {
+      newCart[itemCode] = { qty: 1, price, name, size, variant };
+    }
+    setCart(newCart);
+    saveCart(newCart);
+  };
+
   return (
     <>
       <Navbar />
