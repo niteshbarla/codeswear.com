71e3e48c17ec590137b87015f8d99d0ee8adbf81
Nitesh Barla <nitesh.triptrova2019@gmail.com>
2025-08-13 22:55:18 +0530

    Updated the order file

--- src/pages/order.js
+++ src/pages/order.js
@@ -12,6 +12,17 @@ const Order = () => {
             <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">
               Order Id: #89777
             </h1>
+            <div className="flex mb-4">
+              <a className="flex-grow text-green-500 py-2 text-lg px-1">
+                Iteam Description
+              </a>
+              <a className="flex-grow border-gray-300 py-2 text-lg px-1">
+                Quatity
+              </a>
+              <a className="flex-grow border-gray-300 py-2 text-lg px-1">
+                Item Total
+              </a>
+            </div>
             <p className="leading-relaxed mb-4">
               Your oder has been successfully places. Thank you for shopping
               with us.
@@ -19,36 +30,24 @@ const Order = () => {
             <div className="flex border-t border-gray-200 py-2">
               <span className="text-gray-500">Wear the Code (XL/Blck)</span>
               <span className="ml-auto text-gray-900">1</span>
-              <span className="ml-auto text-gray-900">499</span>
+              <span className="ml-auto text-gray-900">₹499</span>
             </div>
             <div className="flex border-t border-gray-200 py-2">
               <span className="text-gray-500">Wear the Code (XL/Blck)</span>
               <span className="ml-auto text-gray-900">1</span>
-              <span className="ml-auto text-gray-900">499</span>
+              <span className="ml-auto text-gray-900">₹499</span>
             </div>
             <div className="flex border-t border-b mb-6 border-gray-200 py-2">
               <span className="text-gray-500">Wear the Code (XL/Blck)</span>
               <span className="ml-auto text-gray-900">1</span>
-              <span className="ml-auto text-gray-900">499</span>
+              <span className="ml-auto text-gray-900">₹499</span>
             </div>
             <div className="flex">
               <span className="title-font font-medium text-2xl text-gray-900">
-                SubTotal: $58.00
+                SubTotal: ₹1158.00
               </span>
               <button className="flex ml-auto text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded cursor-pointer">
-                Button
-              </button>
-              <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                <svg
-                  fill="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                </svg>
+                Track Order
               </button>
             </div>
           </div>
