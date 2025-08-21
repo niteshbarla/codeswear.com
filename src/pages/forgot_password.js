be10c9183857793dee6eda54b9b794a55b2bb729
Nitesh Barla <nitesh.triptrova2019@gmail.com>
2025-08-21 22:07:11 +0530

    Updated the forgot_password and signup and login pages

---
+++ src/pages/forgot_password.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const forgot_password = () => {
+  return <div>forgot_password</div>;
+};
+
+export default forgot_password;
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,117 @@
 import React from "react";
+import Link from "next/link";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          src="/logo/codeswear_logo_circular/logo-png.png"
+          alt="Your Company"
+          className="mx-auto h-32 w-auto"
+        />
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-black">
+          Sign in to your account
+        </h2>
+        <div className="mt-0 text-center text-sm/6 text-gray-400">
+          Or{" "}
+          <Link
+            href="/signup"
+            className="font-semibold text-green-400 hover:text-green-300"
+          >
+            Signup
+          </Link>
+        </div>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form action="#" method="POST" className="space-y-6">
+          <div>
+            <label
+              for="email"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                type="email"
+                name="email"
+                required
+                autocomplete="email"
+                className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-black/35 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-green-500 sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                for="password"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                type="password"
+                name="password"
+                required
+                autocomplete="current-password"
+                className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-black/35 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-green-500 sm:text-sm/6"
+              />
+            </div>
+            <div className="flex items-center justify-between mt-4">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 text-green-500 focus:ring-green-400 border-gray-300 rounded"
+                />
+                <label
+                  for="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+              <div className="text-sm">
+                <Link
+                  href="/forgot_password"
+                  className="font-semibold text-green-400 hover:text-green-300"
+                >
+                  Forgot password?
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-green-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-green-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-500 cursor-pointer"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm/6 text-gray-400">
+          Not a member?
+          <a
+            href="#"
+            className="font-semibold text-green-400 hover:text-green-300"
+          >
+            Start a 14 day free trial
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
