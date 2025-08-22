3095dc8d9729995fdc5d1f373131b298eb7e8181
Nitesh Barla <nitesh.triptrova2019@gmail.com>
2025-08-22 20:31:18 +0530

    Updated the login, sigin, sign up and forgot password pages

--- src/pages/forgot_password.js
+++ src/pages/forgot_password.js
@@ -1,7 +1,67 @@
 import React from "react";
+import Link from "next/link";
 
 const forgot_password = () => {
-  return <div>forgot_password</div>;
+  return (
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          src="/logo/codeswear_logo_circular/logo-png.png"
+          alt="Your Company"
+          className="mx-auto h-32 w-auto"
+        />
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-black">
+          Forgot Password
+        </h2>
+        <div className="mt-0 text-center text-sm/6 text-gray-400">
+          Or{" "}
+          <Link
+            href="/login"
+            className="font-semibold text-green-400 hover:text-green-300"
+          >
+            Login
+          </Link>
+        </div>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form action="#" method="POST" className="space-y-6">
+          <div>
+            <div className="mt-2">
+              <input
+                id="email"
+                type="email"
+                name="email"
+                required
+                autocomplete="email"
+                className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-black/35 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-green-500 sm:text-sm/6"
+                placeholder="Enter your email"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-green-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-green-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-500 cursor-pointer"
+            >
+              Continue
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm/6 text-gray-400">
+          Not a member?
+          <a
+            href="#"
+            className="font-semibold text-green-400 hover:text-green-300"
+          >
+            Start a 14 day free trial
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default forgot_password;
--- src/pages/login.js
+++ src/pages/login.js
@@ -27,12 +27,6 @@ const Login = () => {
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form action="#" method="POST" className="space-y-6">
           <div>
-            <label
-              for="email"
-              className="block text-sm/6 font-medium text-gray-900"
-            >
-              Email address
-            </label>
             <div className="mt-2">
               <input
                 id="email"
@@ -41,19 +35,12 @@ const Login = () => {
                 required
                 autocomplete="email"
                 className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-black/35 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-green-500 sm:text-sm/6"
+                placeholder="Enter your email"
               />
             </div>
           </div>
 
           <div>
-            <div className="flex items-center justify-between">
-              <label
-                for="password"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Password
-              </label>
-            </div>
             <div className="mt-2">
               <input
                 id="password"
@@ -62,6 +49,7 @@ const Login = () => {
                 required
                 autocomplete="current-password"
                 className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-black/35 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-green-500 sm:text-sm/6"
+                placeholder="Enter your password"
               />
             </div>
             <div className="flex items-center justify-between mt-4">
