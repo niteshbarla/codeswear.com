1e327f42a2ddccadf455560754ef291f9258fd39
Nitesh Barla <nitesh.triptrova2019@gmail.com>
2025-08-25 22:27:43 +0530

    Created middleware folder and created the mongodb connection

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true }, 
   },
   { timestamps: true }
 );
