850d15a7a4bd72149a5310000979241ff3690bc5
Nitesh Barla <nitesh.triptrova2019@gmail.com>
2025-11-07 20:53:00 +0530

    Updated the tshired page

--- src/pages/tshirts.js
+++ src/pages/tshirts.js
@@ -36,32 +36,51 @@ const Tshirts = ({ products }) => {
                         <p className="mt-1">₹{products[item].price}</p>
                         <div className="mt-1">
                           {products[item].size.includes("S") && (
-                            <span className="border border-gray-600 px-1 mx-1">
+                            <span className="border border-gray-300 px-1 mx-1">
                               S
                             </span>
                           )}{" "}
                           {products[item].size.includes("M") && (
-                            <span className="border border-gray-600 px-1 mx-1">
+                            <span className="border border-gray-300 px-1 mx-1">
                               M
                             </span>
                           )}{" "}
                           {products[item].size.includes("L") && (
-                            <span className="border border-gray-600 px-1 mx-1">
+                            <span className="border border-gray-300 px-1 mx-1">
                               L
                             </span>
                           )}{" "}
                           {products[item].size.includes("XL") && (
-                            <span className="border border-gray-600 px-1 mx-1">
+                            <span className="border border-gray-300 px-1 mx-1">
                               XL
                             </span>
                           )}
                           {products[item].size.includes("XXL") && (
-                            <span className="border border-gray-600 px-1 mx-1">
+                            <span className="border border-gray-300 px-1 mx-1">
                               XXL
                             </span>
                           )}
                         </div>
-                        <div className="mt-1">S, M, L, XL, XXL</div>
+                        <div className="mt-1">
+                          {products[item].color.includes("red") && (
+                            <button className="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                          )}
+                          {products[item].color.includes("blue") && (
+                            <button className="border-2 border-gray-300 ml-1 bg-blue-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                          )}
+                          {products[item].color.includes("black") && (
+                            <button className="border-2 border-gray-300 ml-1 bg-black rounded-full w-6 h-6 focus:outline-none"></button>
+                          )}
+                          {products[item].color.includes("green") && (
+                            <button className="border-2 border-gray-300 ml-1 bg-green-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                          )}
+                          {products[item].color.includes("yellow") && (
+                            <button className="border-2 border-gray-300 ml-1 bg-yellow-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                          )}
+                          {products[item].color.includes("purple") && (
+                            <button className="border-2 border-gray-300 ml-1 bg-purple-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                          )}
+                        </div>
                       </div>
                     </a>
                   </Link>
