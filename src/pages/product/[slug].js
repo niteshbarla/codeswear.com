3598df9b85ef549dd6c4e38e815daa65f9cd3b67
Nitesh Barla <nitesh.triptrova2019@gmail.com>
2026-01-18 09:01:14 +0530

    Updated the slug.js

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -99,6 +99,7 @@ export default function Page({ addToCart, product, variants }) {
                 {product.title} ({size}/{color})
               </h1>
               <div className="flex mb-4">
+                {/* 
                 <span className="flex items-center">
                   <svg
                     fill="currentColor"
@@ -158,6 +159,8 @@ export default function Page({ addToCart, product, variants }) {
                   <span className="text-gray-600 ml-3">4 Reviews</span>
                 </span>
                 <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
+
+
                   <a className="text-gray-500">
                     <svg
                       fill="currentColor"
@@ -194,7 +197,7 @@ export default function Page({ addToCart, product, variants }) {
                       <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                     </svg>
                   </a>
-                </span>
+                </span> */}
               </div>
               <p className="leading-relaxed">{product.desc}</p>
               <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
@@ -304,7 +307,7 @@ export default function Page({ addToCart, product, variants }) {
                 <button className="flex ml-4 text-sm text-white bg-green-500 border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-green-600 rounded cursor-pointer">
                   Buy now
                 </button>
-                <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                {/* <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                   <svg
                     fill="currentColor"
                     strokeLinecap="round"
@@ -315,7 +318,7 @@ export default function Page({ addToCart, product, variants }) {
                   >
                     <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
                   </svg>
-                </button>
+                </button> */}
               </div>
               <div className="pin mt-6 flex space-x-2 text-sm">
                 <input
