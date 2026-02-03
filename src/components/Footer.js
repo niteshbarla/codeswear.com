5de550c448c55eb7f07c5a573e465297870ef740
Nitesh Barla <nitesh.triptrova2019@gmail.com>
2026-02-03 08:22:17 +0530

    Updated the footer texts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,14 +22,16 @@ const Footer = () => {
               />
             </Link>
             <p className="mt-2 text-sm text-gray-500">
-              Wear the code &lt;code/&gt; Premium coding t-shirts, hoodies, and
-              apparals.
+              Wear the code &lt;code/&gt;
+            </p>
+            <p className="text-sm text-gray-500">
+              Premium coding t-shirts, hoodies, and apparals.
             </p>
           </div>
           <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-                Shop
+                SHOP
               </h2>
               <nav className="list-none mb-10">
                 <li>
@@ -48,7 +50,7 @@ const Footer = () => {
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-                CATEGORIES
+                POLICY
               </h2>
               <nav className="list-none mb-10">
                 <li>
@@ -75,7 +77,7 @@ const Footer = () => {
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-                CATEGORIES
+                SOCIAL
               </h2>
               <nav className="list-none mb-10">
                 <li>
@@ -102,7 +104,7 @@ const Footer = () => {
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-                CATEGORIES
+                ABOUT
               </h2>
               <nav className="list-none mb-10">
                 <li>
